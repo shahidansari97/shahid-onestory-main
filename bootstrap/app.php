59739bf03abb2a76beafb6dc4e681baf13e0e52f
shahidansari97 <shahid@nextolive.com>
2026-06-03 21:11:31 +0530

    New Test for

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -35,5 +35,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //ss
+        //ssshah
     })->create();
