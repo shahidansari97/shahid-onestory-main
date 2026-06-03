c23ff864caedd2633c9c929db668fbbe298948a8
shahidansari97 <shahid@nextolive.com>
2026-06-03 21:03:22 +0530

    1 modules

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -35,5 +35,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //s
+        //ss
     })->create();
